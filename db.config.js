eeeb65152ae09630bccb8d77a46597ea717085ce
Sebastian Liebentraut <basti.zfjk@posteo.de>
2023-04-23 19:51:22 +0200

    erase db config from git ignore

---
+++ db.config.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+
+const options = {
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+async function connection() {
+  return await mysql.createConnection(options);
+}
+
+exports.connection = connection;
